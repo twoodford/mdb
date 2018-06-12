5f0a3cca6d9576d22d54a24269270f21c4707d20
Tim Woodford <timthewoodford@gmail.com>
2018-06-11 21:59:08 -0400

    Move graph walking to central location

--- mdb/songgraph.py
+++ mdb/songgraph.py
@@ -1,7 +1,8 @@
 # songgraph.py (mdb.songgraph)
-# Copyright (C) 2014 Timothy Woodford.  All rights reserved.
+# Copyright (C) 2014-2017 Timothy Woodford.  All rights reserved.
 # Create song-related graph structures
 from datetime import timedelta
+import random
 import networkx as nx
 
 def make_play_graph(sdb, grtype=nx.MultiDiGraph):
@@ -24,6 +25,7 @@ def make_play_graph(sdb, grtype=nx.MultiDiGraph):
         prev = row
     return gr
     
+# TODO this is inherently sub-optimal - need a better way to do find a route
 def possible_routes(start_key, graph, maxdepth, _visited=None):
     """ Function to find all possible routes that we could take along the given graph, 
         starting at the song given by start_key, up to a certain maximum number of songs 
@@ -33,13 +35,34 @@ def possible_routes(start_key, graph, maxdepth, _visited=None):
     if _visited is None:
         _visited = list()
     if maxdepth == 1:
-        ret = [[song] for song in graph.successors(start_key)]
+        ret = [[song] for song in graph.successors(start_key) if not song in _visited]
     else:
         _visited.append(start_key)
         ret = list()
         for song in graph.successors(start_key):
             if not song in _visited:
                 ret.extend([[start_key] + route for route in possible_routes(song, graph, maxdepth - 1, _visited)])
-    if len(ret) == 0:
-        ret.append([start_key])
     return ret
+
+class _emptydict(object):
+    def __getitem__(self, index):
+        return 1
+
+def graph_walk(start_key, graph, song_weight=_emptydict(), max_depth=15, iter_depth=3, mean_selection_index=5):
+    sequence = [start_key]
+    last = start_key
+    lmbda=1/mean_selection_index
+    while len(sequence) < max_depth:
+        options = possible_routes(last, graph, iter_depth, _visited=list(sequence))
+        def _lstdens(lst):
+            return sum([song_weight[key] for key in lst])
+        options.sort(key=_lstdens, reverse=True)
+        if len(options)==0:
+            break
+        # Bias selection towards things that are earlier in list
+        choice = options[min(round(random.expovariate(lmbda)), len(options)-1)]
+        if len(choice)==0:
+            break
+        sequence.append(choice[1])
+        last = sequence[-1]
+    return sequence
--- simplesuggest.py
+++ simplesuggest.py
@@ -1,9 +1,7 @@
-#!/opt/local/bin/python3.5
+#!/opt/local/bin/python3.6
 # simplesuggest.py
 # Copyright (C) 2014 Timothy Woodford.  All rights reserved.
 from __future__ import print_function
-from datetime import datetime, time
-import random
 import sys
 import sqlite3
 
@@ -11,21 +9,10 @@ import mdb.seed
 import mdb.util
 import mdb.songgraph
 
-def proc_adjacent_graph(sel_sid, graph, sdb):
-    possible = mdb.songgraph.possible_routes(sel_sid, graph, 10)
-    dens = mdb.seed.play_density(sdb)
-    def _lstdens(lst):
-        return sum([dens[key] for key in lst])
-    possible.sort(key=_lstdens, reverse=True)
-    # Randomly choose a sequence, with a bias towards stuff at the beginning
-    rngmod = len(possible)//2 + 1
-    for lst in possible:
-        if random.randrange(rngmod) == 0:
-            return lst
-
 if __name__=="__main__":
     sdb = sqlite3.connect("data/music.sqlite3")
     graph = mdb.songgraph.make_play_graph(sdb)
+    dens = mdb.seed.play_density(sdb)
     if len(sys.argv)<=1:
         for key in mdb.seed.get_start_points(sdb):
             sel_song = key
@@ -33,5 +20,5 @@ if __name__=="__main__":
                 break
     else:
         sel_song = mdb.util.sname_to_key(sys.argv[1], sdb)
-    slist = proc_adjacent_graph(sel_song, graph, sdb)
+    slist = mdb.songgraph.graph_walk(sel_song, graph, dens)
     [print(mdb.util.key_to_string(key, sdb)) for key in slist]
