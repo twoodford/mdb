649ae7ba0ff0ab0710b9bdf067aaffad1a09c39e
Tim Woodford <timthewoodford@gmail.com>
2018-08-29 22:37:27 -0400

    Sync mdbserver code to new seed pipeline

--- mdb/mdbserver.py
+++ mdb/mdbserver.py
@@ -9,16 +9,21 @@ import threading
 import math
 import operator
 
+import mdb.dtutil
 import mdb.seed
 import mdb.util
 import mdb.songgraph
 
 sdb = sqlite3.connect("data/music.sqlite3")
+times_dict = mdb.dtutil.times_dict(sdb)
 weather_db = sqlite3.connect("data/weather.sqlite3")
 graph = mdb.songgraph.make_play_graph(sdb)
-pdens = mdb.seed.play_density(sdb)
+pdens = mdb.seed.play_times_density(times_dict)
 wdens = mdb.seed.get_weather_dens(weather_db, sdb)
+rateadj = mdb.seed.rating_adjustment(sdb, weight=0.8)
+playadj = mdb.seed.num_plays_adjustment(times_dict, weight=0.6)
 combo_dens = {}
+
 
 def update_pdens(schedu, sdb_tl, wdb_tl):
     global wdens
@@ -28,18 +33,23 @@ def update_pdens(schedu, sdb_tl, wdb_tl):
     if sdb_tl is None or wdb_tl is None:
         sdb_tl = sqlite3.connect("data/music.sqlite3")
         wdb_tl = sqlite3.connect("data/weather.sqlite3")
-    pdens = mdb.seed.play_density(sdb_tl)
+    st = time.time()
+    pdens = mdb.seed.play_times_density(times_dict)
     wdens = mdb.seed.get_weather_dens(wdb_tl, sdb_tl)
-    combo_dens = {sid: (weather_dens * math.sqrt(pdens[sid])) for (sid, weather_dens) in wdens}
+    combo_dens_sc = mdb.seed.keywise_mult(wdens, pdens)
+    combo_dens_sc = mdb.seed.keywise_mult(combo_dens_sc, rateadj)
+    combo_dens_sc = mdb.seed.keywise_mult(combo_dens_sc, playadj)
     for key in pdens:
-        if key not in combo_dens:
-            combo_dens[key] = pdens[key] * 0.02
-    print("done")
+        if key not in combo_dens_sc:
+            combo_dens_sc[key] = pdens[key] * 0.02
+    # Don't want to modify global in place
+    combo_dens = combo_dens_sc
+    print("done", time.time()-st)
     schedu.enter(60, 0, update_pdens, (schedu, sdb_tl, wdb_tl))
 
 def update_pdens_thread():
     schedu = sched.scheduler(time.time, time.sleep)
-    schedu.enter(20, 0, update_pdens, (schedu, None, None))
+    schedu.enter(2, 0, update_pdens, (schedu, None, None))
     schedu.run()
 
 
@@ -58,7 +68,7 @@ class MDBRequestHandler(http.server.SimpleHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(self._encode_song_list(songkeys))
         elif self.path=="/weatherseed":
-            seed = sorted([(sid, weather_dens * math.sqrt(pdens[sid])) for (sid, weather_dens) in wdens[:100]], key=operator.itemgetter(1), reverse=True)
+            seed = sorted([(key, combo_dens[key]) for key in combo_dens], key=operator.itemgetter(1), reverse=True)
             songkeys = [x[0] for x in seed[:20]]
             self.send_response(200)
             self.end_headers()
