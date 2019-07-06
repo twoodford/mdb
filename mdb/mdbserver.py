6126b590a196b25e36af92e24eac0cc49552c85a
Tim Woodford <timthewoodford@gmail.com>
2019-07-06 15:15:46 -0700

    Make mdbserver data accessible via ajax

--- mdb/mdbserver.py
+++ mdb/mdbserver.py
@@ -1,4 +1,5 @@
 import http.server
+import http.client
 import socketserver
 import sqlite3
 import random
@@ -8,22 +9,18 @@ import time
 import threading
 import math
 import operator
+import urllib.parse
 
-import mdb.dtutil
 import mdb.seed
 import mdb.util
 import mdb.songgraph
 
 sdb = sqlite3.connect("data/music.sqlite3")
-times_dict = mdb.dtutil.times_dict(sdb)
 weather_db = sqlite3.connect("data/weather.sqlite3")
 graph = mdb.songgraph.make_play_graph(sdb)
-pdens = mdb.seed.play_times_density(times_dict)
+pdens = mdb.seed.play_density(sdb)
 wdens = mdb.seed.get_weather_dens(weather_db, sdb)
-rateadj = mdb.seed.rating_adjustment(sdb, weight=0.8)
-playadj = mdb.seed.num_plays_adjustment(times_dict, weight=0.6)
 combo_dens = {}
-
 
 def update_pdens(schedu, sdb_tl, wdb_tl):
     global wdens
@@ -33,23 +30,18 @@ def update_pdens(schedu, sdb_tl, wdb_tl):
     if sdb_tl is None or wdb_tl is None:
         sdb_tl = sqlite3.connect("data/music.sqlite3")
         wdb_tl = sqlite3.connect("data/weather.sqlite3")
-    st = time.time()
-    pdens = mdb.seed.play_times_density(times_dict)
+    pdens = mdb.seed.play_density(sdb_tl)
     wdens = mdb.seed.get_weather_dens(wdb_tl, sdb_tl)
-    combo_dens_sc = mdb.seed.keywise_mult(wdens, pdens)
-    combo_dens_sc = mdb.seed.keywise_mult(combo_dens_sc, rateadj)
-    combo_dens_sc = mdb.seed.keywise_mult(combo_dens_sc, playadj)
+    combo_dens = {sid: (weather_dens * math.sqrt(pdens[sid])) for (sid, weather_dens) in wdens}
     for key in pdens:
-        if key not in combo_dens_sc:
-            combo_dens_sc[key] = pdens[key] * 0.02
-    # Don't want to modify global in place
-    combo_dens = combo_dens_sc
-    print("done", time.time()-st)
+        if key not in combo_dens:
+            combo_dens[key] = pdens[key] * 0.02
+    print("done")
     schedu.enter(60, 0, update_pdens, (schedu, sdb_tl, wdb_tl))
 
 def update_pdens_thread():
     schedu = sched.scheduler(time.time, time.sleep)
-    schedu.enter(2, 0, update_pdens, (schedu, None, None))
+    schedu.enter(20, 0, update_pdens, (schedu, None, None))
     schedu.run()
 
 
@@ -61,22 +53,30 @@ class MDBRequestHandler(http.server.SimpleHTTPRequestHandler):
         songs = [[mdb.util.get_key_sname(key, sdb), mdb.util.get_key_artist(key, sdb), key] for key in songkeys]
         return json.dumps(songs).encode("utf-8")
 
+    def _do_ok(self):
+        self.send_response(200)
+        # Allow Ajax requests from locally-run servers, but nowhere else
+        if urllib.parse.urlparse(str(self.headers["Origin"])).netloc.split(":")[0] == "localhost":
+            self.send_header("Access-Control-Allow-Origin", self.headers["Origin"])
+        self.send_header('Content-Type', 'application/json')
+
     def do_GET(self):
         if self.path=="/simpleseed":
             songkeys = mdb.seed.get_start_points(sdb)[:20]
-            self.send_response(200)
+            self._do_ok()
             self.end_headers()
             self.wfile.write(self._encode_song_list(songkeys))
         elif self.path=="/weatherseed":
-            seed = sorted([(key, combo_dens[key]) for key in combo_dens], key=operator.itemgetter(1), reverse=True)
+            print(self.headers)
+            seed = sorted([(sid, weather_dens * math.sqrt(pdens[sid])) for (sid, weather_dens) in wdens[:100]], key=operator.itemgetter(1), reverse=True)
             songkeys = [x[0] for x in seed[:20]]
-            self.send_response(200)
+            self._do_ok()
             self.end_headers()
             self.wfile.write(self._encode_song_list(songkeys))
         elif self.path.startswith("/graphcrawl"):
             sel_song = int(self.path.split("?")[1])
             lst = mdb.songgraph.graph_walk(sel_song, graph, combo_dens)
-            self.send_response(200)
+            self._do_ok()
             self.end_headers()
             self.wfile.write(self._encode_song_list(lst))
         else:
