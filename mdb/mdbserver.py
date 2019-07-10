2a5cb5cff26506d5c00e93b271be4f816a848afa
Tim Woodford <timthewoodford@gmail.com>
2019-07-09 21:50:50 -0700

    Bugfixes for mdbserver
    Reduce time until first update from 20 seconds
    Fix error in generation of combo_dens

--- mdb/mdbserver.py
+++ mdb/mdbserver.py
@@ -32,7 +32,7 @@ def update_pdens(schedu, sdb_tl, wdb_tl):
         wdb_tl = sqlite3.connect("data/weather.sqlite3")
     pdens = mdb.seed.play_density(sdb_tl)
     wdens = mdb.seed.get_weather_dens(wdb_tl, sdb_tl)
-    combo_dens = {sid: (weather_dens * math.sqrt(pdens[sid])) for (sid, weather_dens) in wdens}
+    combo_dens = {sid: (weather_dens * math.sqrt(pdens[sid])) for (sid, weather_dens) in wdens.items()}
     for key in pdens:
         if key not in combo_dens:
             combo_dens[key] = pdens[key] * 0.02
@@ -41,7 +41,7 @@ def update_pdens(schedu, sdb_tl, wdb_tl):
 
 def update_pdens_thread():
     schedu = sched.scheduler(time.time, time.sleep)
-    schedu.enter(20, 0, update_pdens, (schedu, None, None))
+    schedu.enter(1, 0, update_pdens, (schedu, None, None))
     schedu.run()
 
 
@@ -68,7 +68,7 @@ class MDBRequestHandler(http.server.SimpleHTTPRequestHandler):
             self.wfile.write(self._encode_song_list(songkeys))
         elif self.path=="/weatherseed":
             print(self.headers)
-            seed = sorted([(sid, weather_dens * math.sqrt(pdens[sid])) for (sid, weather_dens) in wdens[:100]], key=operator.itemgetter(1), reverse=True)
+            seed = sorted([(sid, weather_dens * math.sqrt(pdens[sid])) for (sid, weather_dens) in wdens.items()], key=operator.itemgetter(1), reverse=True)
             songkeys = [x[0] for x in seed[:20]]
             self._do_ok()
             self.end_headers()
